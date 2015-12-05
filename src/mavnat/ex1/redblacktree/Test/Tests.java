c77d45f7a4144fad366656bd13d56377a056ccd2
Daniel Feldman <feldman.daniel@gmail.com>
2015-12-05 22:46:13 +0200

    doc changes

--- src/mavnat/ex1/redblacktree/Test/Tests.java
+++ src/mavnat/ex1/redblacktree/Test/Tests.java
@@ -18,7 +18,7 @@ import mavnat.ex1.redblacktree.Test.Utils.*;
 
 /****
  * !!!! README !!!!
- * In order to use this test file - You must 
+ * In order to use this test file - You must complete 3 steps: 
  * 
  * A) implement function in both RBTree and RBNode as follow:
  * 	
@@ -33,12 +33,12 @@ import mavnat.ex1.redblacktree.Test.Utils.*;
  *			4) public RBTree.RBNode getParent() //Not exist in the given skeleton
  *
  * B) Add to the project the JUnit library - it is a built in library for automatic testing:
- * 					Click on Project -> Properties, select Java Build Path, Libraries -> Add Library, 
- * 																				then choose JUnit -> next + finish
+ * 					Click on "Project" -> "Properties", select "Java Build Path", "Libraries" -> "Add Library", 
+ * 																				then choose "JUnit" -> next + finish
  * 
- * C) Your RBTree should be inside mavant package, ( == mavnat folder)
+ * C) 1) Your RBTree should be inside mavant package, ( == mavnat folder)
  * 								or you may change all the "package mavnat." declerations to your package name 
- * 	  and Test Folder should be next to mavant folder;
+ * 	  2) Test Folder should be next to mavant folder;
  * 
  * 
  * Enjoy and Good Luck!
